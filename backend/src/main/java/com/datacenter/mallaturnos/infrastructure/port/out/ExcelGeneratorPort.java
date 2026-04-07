30b3b4d29cbad20c79f418363dfeb8dcd5b5f091
DavidLugoDC <david.lugo@dcsas.com.co>
2026-04-07 15:40:22 -0500

    Prueba 1

--- backend/src/main/java/com/datacenter/mallaturnos/infrastructure/port/out/ExcelGeneratorPort.java
+++ backend/src/main/java/com/datacenter/mallaturnos/infrastructure/port/out/ExcelGeneratorPort.java
@@ -2,10 +2,11 @@ package com.datacenter.mallaturnos.infrastructure.port.out;
 
 import com.datacenter.mallaturnos.application.Dto.AsignacionTurno.AsignacionTurnoDto;
 import java.util.List;
+import java.util.Map;
 
 public interface ExcelGeneratorPort {
 
-    byte[] generarExcelAsignaciones(List<AsignacionTurnoDto> asignaciones);
+    byte[] generarExcelAsignaciones(List<AsignacionTurnoDto> asignaciones, Map<Long, String> funcionarioNames, Map<Long, String> turnoNames);
 
-    byte[] generarExcelAsignacionesPorUsuario(String nombreUsuario, List<AsignacionTurnoDto> asignaciones);
+    byte[] generarExcelAsignacionesPorUsuario(String nombreUsuario, List<AsignacionTurnoDto> asignaciones, Map<Long, String> turnoNames);
 }
